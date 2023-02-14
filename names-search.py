e4e619447f456191eb3b14453afbe0e35ae5f2e2
unknown <UV@FNT-AH07.orizaba.intra.uv.mx>
2023-02-14 09:56:21 -0600

    add python streamlit programs

---
+++ names-search.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Search names")
+DATA_URL = "dataset.csv"
+
+@st.cache
+def load_data_byname(name):
+    data = pd.read_csv(DATA_URL) # read csv
+    filtered_data_byname = data[data["name"].str.contains(name)]
+    return filtered_data_byname  # return the dataframe
+
+
+myname = st.text_input("nombre :")
+if (myname):
+    filterbyname = load_data_byname(myname)  # call the function
+    count_row = filterbyname.shape[0]  # count number of records
+    st.write(f"Total names : {count_row}")
+    
+    st.dataframe(filterbyname)
+
---
+++ welcome.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def bienvenida(nombre):
+    mymensaje = "bienvenido /a/e " + nombre
+    return mymensaje
+
+myname = st.text_input("nombre :")
+if (myname):
+    mensaje = bienvenida(myname)
+    st.write(f" Result : {mensaje}")
+    
+
